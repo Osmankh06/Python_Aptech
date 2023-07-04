c44572dfb02a8a8f906d2ae0398e9571b06fe80b
Osmankh06 <137781221+Osmankh06@users.noreply.github.com>
2023-07-05 00:14:21 +0500

    Add files via upload

---
+++ Assignments/Assignment 3.py
@@ -0,0 +1,11 @@
+# num1 = [2, 4, 6]
+# num2 = [2, 4, 6]
+# for i in range(0, 2):
+#     print(num1[i]+num2[i],", ",end="")
+# print(num1[2]+num2[2])
+
+
+num1 = [2, 4, 6]
+num2 = [2, 4, 6]
+for i in range(0, 3):
+    print(num1[i]+num2[i],", ",end="")
