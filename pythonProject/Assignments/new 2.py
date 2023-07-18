f1a0662cfd4b57a78780d6d9c12711930ed90bad
Osmankh06 <osman17dec2006@gmail.com>
2023-07-18 21:40:14 +0500

    ⠀

---
+++ pythonProject/Assignments/new 2.py
@@ -0,0 +1,6 @@
+vowels = ["a", "e", "i", "o", "u"]
+alpha = input("Enter any alphabet:")
+if alpha in vowels:
+    print("The given alphabet is a vowel")
+else:
+    print("The given alphabet is not a vowel")
