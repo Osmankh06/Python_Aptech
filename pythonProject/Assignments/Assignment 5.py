189954c5941cc07e8a7da04a8a32de5c6e4f1787
Osmankh06 <osman17dec2006@gmail.com>
2023-07-24 00:36:47 +0500

    ⠀

--- pythonProject/Assignments/Assignment 5.py
+++ pythonProject/Assignments/Assignment 5.py
@@ -1,24 +1,3 @@
-# from tkinter import *
-# root = Tk()
-# root.geometry("300x300")
-# a = Label(root,text ="Enter Your Name=").grid(row=0,column=0)
-# b = Entry(root,width=50).grid(row =0, column =1)
-# def Click():
-#     d = Label(root, text=b.get()).grid(row=1,column=1)
-#
-# c = Button(root,text="Submit",command=Click).grid(row=2,column=1)
-#
-#
-# # A.pack()
-# # def Click():
-# #     C = Label(root, text=A.get())
-# #     C.pack()
-# #
-# #
-# # B = Button(root,text="Submit", command=Click)
-# # B.pack()
-# root.mainloop()
-
 from tkinter import *
 root = Tk()
 root.geometry()
