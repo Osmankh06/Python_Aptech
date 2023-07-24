e3d64a91c8bef4b6a625b76bbc5b6ee2a8a55c2c
Osmankh06 <osman17dec2006@gmail.com>
2023-07-24 21:38:35 +0500

    ⠀

--- pythonProject/Assignments/Assignment 5.py
+++ pythonProject/Assignments/Assignment 5.py
@@ -1,18 +1,21 @@
 from tkinter import *
+
 root = Tk()
 root.geometry()
 a = Label(root, text="Enter Your Name:")
 a.grid(row=0, column=0)
-b = Entry(root)
+b = Entry(root, width=20)
 b.grid(row=0, column=1)
+
+
 def Click():
-    d = Label(root,text=b.get())
-    d.grid(row=1,column=1)
+    d = Label(root, text=b.get(), width="20")
+    d.grid(row=1, column=1)
 
 
-d = Label(root, text="None")
+d = Label(root, text="None", width=20)
 d.grid(row=1, column=1)
-c = Button(root, text="Submit",command=Click)
+c = Button(root, text="Submit", command=Click)
 c.grid(row=2, column=1)
 root.mainloop()
 
